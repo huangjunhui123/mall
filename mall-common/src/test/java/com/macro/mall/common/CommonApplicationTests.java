5d1e75097285c7c30590b17234538302d90a3e62
hanyuntao <hanyuntaocn@163.com>
2021-12-07 18:52:07 +0800

    新增mall-common工具类

---
+++ mall-common/src/test/java/com/macro/mall/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macro.mall.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
